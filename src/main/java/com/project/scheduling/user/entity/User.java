b4c3c97ac11be9249ea86aa96fe1fa3fd7be4212
tjrqh <asdww6631@gmail.com>
2025-01-23 14:09:53 +0900

    feat: modify entity

--- src/main/java/com/project/scheduling/user/entity/User.java
+++ src/main/java/com/project/scheduling/user/entity/User.java
@@ -2,8 +2,13 @@ package com.project.scheduling.user.entity;
 
 
 import com.project.scheduling.baseentity.BaseEntity;
-import jakarta.persistence.*;
-import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
