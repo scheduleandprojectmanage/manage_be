e919a35802192c134a2deb370374e232d1350860
MJwith4Cats <life.exhib@gmail.com>
2025-01-23 11:51:27 +0900

    feat: 예외처리 추가

--- src/main/java/com/project/scheduling/user/entity/User.java
+++ src/main/java/com/project/scheduling/user/entity/User.java
@@ -3,6 +3,9 @@ package com.project.scheduling.user.entity;
 
 import com.project.scheduling.baseentity.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import lombok.*;
 
@@ -17,8 +20,18 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String password;
+
+    @Column(unique = true)
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    @NotBlank(message = "이메일은 필수입니다")
     private String email;
+
+//    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+//            message = "비밀번호는 8~20자리이며 영문, 숫자, 특수문자를 포함해야 합니다")
+    @NotBlank(message = "비밀번호는 필수입니다")
+    private String password;
+
+    @NotBlank(message = "이름은 필수입니다")
     private String name;
 //    private String provider; //자사 가입인지, 소셜 회원인지 구분
 
