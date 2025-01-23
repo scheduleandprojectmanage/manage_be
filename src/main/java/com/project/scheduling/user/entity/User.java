2bc8c3134865c0b1264caada6ba34621f91a7f9f
tjrqh <asdww6631@gmail.com>
2025-01-23 13:47:08 +0900

    feat : crete entity

--- src/main/java/com/project/scheduling/user/entity/User.java
+++ src/main/java/com/project/scheduling/user/entity/User.java
@@ -3,6 +3,7 @@ package com.project.scheduling.user.entity;
 
 import com.project.scheduling.baseentity.BaseEntity;
 import jakarta.persistence.*;
+import java.util.List;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
